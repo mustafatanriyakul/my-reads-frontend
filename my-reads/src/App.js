ad9ea01b5a90d0bff1ae6be1407ceaadd9574d92
mustafa.tanriyakul <mustafatanriyakul1@gmail.com>
2025-08-22 19:44:55 +0300

    login and register pages implemented

--- my-reads/src/App.js
+++ my-reads/src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import Books from './components/Books';
 import MyBooks from './components/MyBooks';
 import Author from './components/Author';
+import Register from './components/Register';
+import Login from './components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
     <div>
-      <Author />
+      <Register />
     </div>
   );
 }
